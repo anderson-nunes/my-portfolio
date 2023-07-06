ed107ce5e9ecc2c22e467aca06ac0e0638d3a4db
anderson-nunes <andersonribeironunes@hotmail.com>
2023-07-06 15:18:59 -0300

    Estruturando portifólio

--- src/components/layout/navbar/Navbar.js
+++ src/components/layout/navbar/Navbar.js
@@ -1,23 +1,50 @@
 import { Link } from "react-router-dom";
 import styles from "../navbar/Navbar.module.css";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { FiX } from "react-icons/fi";
+import { useState } from "react";
+import Modal from "react-modal"
+
 
 export function Navbar() {
+
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  const openModal = () => {
+    setModalIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setModalIsOpen(false)
+  }
+
   return (
     <nav className={styles.navbar}>
       <div>
-        <h1>anderson</h1>
+        <div className={styles.content}>
+          <h2>anderson</h2>
+          <h2>anderson</h2>
+        </div>
       </div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projetos</Link>
-        </li>
-        <li>
-          <Link to="contact">Contato</Link>
-        </li>
-      </ul>
+      <div className={styles.container}>
+        <HiOutlineMenuAlt3 className={styles.open} onClick={openModal} />
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel="Menu Modal"
+          overlayClassName={styles.modal_overlay}
+          className={styles.moda_content}
+        >
+          <FiX className={styles.close} onClick={closeModal} />
+          <div className={styles.open_modal}>
+            <ul>
+              <li>HOME</li>
+              <li>CONTATO</li>
+              <li>CONTATO</li>
+            </ul>
+          </div>
+        </Modal>
+      </div>
     </nav>
   );
 }
--- src/components/layout/section/Content.js
+++ src/components/layout/section/Content.js
@@ -4,16 +4,60 @@ import foto from '../../../assets/foto.jpg'
 export function Content() {
 
   return (
-    <article className={styles.container}>
-      <div className={styles.container_section}>
-        <div>
-          <h1>Frontend Developer.</h1>
+    <>
+      <section className={styles.container}>
+        <div className={styles.container_section_one}>
+          <div className={styles.container_text_one}>
+            <h1>Frontend
+              <br />
+              Developer.</h1>
+            <p>Gosto de criar produtos de front-end sólidos e escaláveis ​​com ótimas experiências de usuário.</p>
+          </div>
+          <div>
+            {/* <img src={foto} alt="" /> */}
+          </div>
         </div>
-        <div>
-          <img src={foto} alt="" />
+      </section>
+      <section className={styles.container_section_one}>
+        <div className={styles.container_text_one}>
+          <h2>QUEM SOU EU</h2>
+          <span>Desenvolvedor Front-end, com experiência em HTML, CSS, JavaScript e React.js.
+            Meu objetivo é criar interfaces de usuário envolventes e eficientes para sites e
+            aplicativos. Eu valorizo muito a colaboração em equipe e estou sempre
+            buscando aprimorar minhas habilidades técnicas e de comunicação para me
+            tornar um desenvolvedor Front-end ainda melhor.</span>
         </div>
-      </div>
-    </article>
+        <div className={styles.container_text_one}>
+          <h2>MINHAS FORMAÇÕES</h2>
+          <h3>Labenu</h3>
+          <span>
+            BOOTCAMP DE DESENVOLVIMENTO WEB FULL-STACK
+            JANIRO/2023 - JANEIRO/2024
+            Programa com mais de 1000 horas de experiência prática em desenvolvimento
+            Full-stack, guiadas por metodologias ágeis (Kanban/Scrum).
+            Entre as habilidades aprendidas durante o curso estão HTML, CSS, JavaScript,
+            React, Styled-Components, React Hooks, REST, Api Restful, HTTP, Jest, Node.JS,
+            Knex, TypeScript, MySQL, AWS, SQL, Git, Github, Firebase, Testes unitários e UI
+            Design.
+          </span>
+          <h3>QI Faculdade & Escola Técnica</h3>
+          <span>CURSO DE TÉCNICO EM INFORMÁTICA PARA INTERNET
+            FEVEREIRO/2021 - OUTUBRO/2022
+            Neste curso eu aprendi mais sobre o processo de desenvolvimento de
+            software seja ele um aplicativo mobile usando Android ou Flutter, um
+            website usando JavaScript e/ou uma plataforma de checkout com PHP e MySQL
+          </span>
+        </div>
+      </section>
+      <section className={styles.container_section_two}>
+        <div className={styles.container_text_two}>
+          <h2>Ao longo dos
+            <br />
+            anos,</h2>
+        </div>
+
+      </section>
+    </>
   )
 
 }
